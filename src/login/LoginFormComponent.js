44c2233152a759bc1bdc4e69a062b3cc0df54366
yogimahaputra <yogi.mahaputra123@gmail.com>
2023-01-22 14:21:50 +0700

    make test error

--- src/login/LoginFormComponent.js
+++ src/login/LoginFormComponent.js
@@ -18,7 +18,7 @@ const LoginFormComponent = ({ login }) => {
         <form onSubmit={loginAction} method='post' >
             <p className="mb-4">Please login to your account</p>
             <div className="mb-4">
-                <input onChange={handleChange} value={input.email} required name="email" type="email" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Email" />
+                <input onChange={handleChange} value={input.email} required name="email" type="email" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="email" />
             </div>
             <div className="mb-4">
                 <input onChange={handleChange} value={input.password} required name="password" type="password" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Password" />
